6089d602a770195d1d475cc9eb937b167c7c5d2d
JusGaga <gaetan3107@gmail.com>
2022-03-07 23:25:57 +0100

    finish web pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter,Routes,Route} from "react-router-dom"
+
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 import AllHome from "./pages/AllHome";
 import AboutMePages from "./pages/pages/AboutMePages/AboutMePages";
 import ProjectPages from "./pages/pages/ProjectPages/ProjectPages";
 
 
-
 function App() {
-  return (
-    <>
-      <div >
+    return (
+        <>
+            <div>
 
-      <BrowserRouter>
-        <div className="Container">
-          <Routes>
-            <Route exact path="/" element={AllHome()}/>
-            <Route exact path="/Projects" element={ProjectPages()}/>
-            <Route exact path="/AboutMe" element={AboutMePages()}/>
-          </Routes>
-        </div>
-      </BrowserRouter>
-      </div>
-    </>
-  );
+                <BrowserRouter>
+                    <div className="Container">
+                        <Routes>
+                            <Route exact path="/" element={AllHome()}/>
+                            <Route exact path="/Projects" element={ProjectPages()}/>
+                            <Route exact path="/AboutMe" element={AboutMePages()}/>
+                        </Routes>
+                    </div>
+                </BrowserRouter>
+            </div>
+        </>
+    );
 }
 
 export default App;
--- src/pages/pages/StarterPages/Projects.jsx
+++ src/pages/pages/StarterPages/Projects.jsx
@@ -8,39 +8,39 @@ import ProjectImg from '../../../Img/ProjectImg.png';
 import UnderTitle from '../../../Img/UnderTitle.svg';
 
 import Scroll from '../../../components/Scroll';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 
 const Projects = (props) => {
-  return (
-    <>
-      <div className={'planet2'} style={{ display: props.activate === 1 ? 'none' : 'block' }}>
-        <img src={planet2} alt='planet2'/>
-      </div>
-      <div className={'middlePageHome2 '} id={'project'} style={{
-        top: props.activate === 1
-          ? '0vh'
-          : '100vh',
-      }}>
-        <Scroll page={2} activate={props.activate}/>
-        <div className='containerProject'>
-          <div className='Present'>
-            <h1>Projets</h1>
-            <img className='underTitle' src={UnderTitle} alt={'underTitle'}/>
-            <h2>Explorer mes différents projets,<br/> Je vous expliquerai comment je les est
-                réalisé.</h2>
-            <div style={{ display: props.activate === 1 ? 'none' : 'block' }}>
-              <Link to='/Projects' onClick={window.scrollTo(0, 0)}>Explorez plus</Link></div>
-          </div>
-        </div>
-        <img className={'ProjectImg '} src={ProjectImg} alt='Projets'/>
-      </div>
-      <div className={'planet3'} style={{ display: props.activate === 1 ? 'none' : 'flex' }}>
-        <img src={planet3} alt='planet3'/>
-      </div>
-      <div className={'line2'} style={{ display: props.activate === 1 ? 'none' : 'block' }}/>
-    </>
-  );
+    return (
+        <>
+            <div className={'planet2'} style={{display: props.activate === 1 ? 'none' : 'block'}}>
+                <img src={planet2} alt='planet2'/>
+            </div>
+            <div className={'middlePageHome2 '} id={'project'} style={{
+                top: props.activate === 1
+                    ? '0vh'
+                    : '100vh',
+            }}>
+                <Scroll page={2} activate={props.activate}/>
+                <div className='containerProject'>
+                    <div className='Present'>
+                        <h1>Projets</h1>
+                        <img className='underTitle' src={UnderTitle} alt={'underTitle'}/>
+                        <h2>Explorer mes différents projets,<br/> Je vous expliquerai comment je les est
+                            réalisé.</h2>
+                        <div style={{display: props.activate === 1 ? 'none' : 'block'}}>
+                            <Link to='/Projects' onClick={window.scrollTo(0, 0)}>Explorez plus</Link></div>
+                    </div>
+                </div>
+                <img className={'ProjectImg '} src={ProjectImg} alt='Projets'/>
+            </div>
+            <div className={'planet3'} style={{display: props.activate === 1 ? 'none' : undefined}}>
+                <img src={planet3} alt='planet3'/>
+            </div>
+            <div className={'line2'} style={{display: props.activate === 1 ? 'none' : 'block'}}/>
+        </>
+    );
 };
 
 export default Projects;
